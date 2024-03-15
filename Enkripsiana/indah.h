08f82991d8935251279baca9836955d740f520b5
indahrtp <indahpramudita74@gmail.com>
2024-03-15 11:38:15 +0700

    Update Header

--- Enkripsiana/indah.h
+++ Enkripsiana/indah.h
@@ -6,4 +6,10 @@
 #include <stdint.h>
 #include <string.h>
 
+// Modul untuk melakukan operasi AddRoundkey sebanyak 10 kali putaran
+void addRoundKey(aes_state_t* state, const aes_key_t* key)
+
+// Modul untuk melakukan enkripsi dari penggabungan setiap modul 
+void aes128EncryptBlock(&state, &key);
+
 #endif // !INDAH_H

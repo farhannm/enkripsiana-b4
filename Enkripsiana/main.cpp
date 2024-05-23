99f9278d30c2b58e5791142a55328b58adf515b4
febi33q <158978346+febi33q@users.noreply.github.com>
2024-05-23 10:47:03 +0700

    Update main.cpp

--- Enkripsiana/main.cpp
+++ Enkripsiana/main.cpp
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include "enkripsiana.h"
+#include "Decrypt.h"
+#include "Encrypt.h"
+#include "uihead.h"
 
 int main(int argc, char* argv[]) {
     
-
+    banner();
     mainMenu();
 
     return 0;
